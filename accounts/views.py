74da07580ed1b8df6a5ea775a64b9171bae01b54
Mandip <mndpadhikari@gmail.com>
2026-03-16 13:27:11 +0545

    accounts app: models, forms, views and urls completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            messages.error(request, "Invalid information please check and provided correct data.")
+            return render(request, 'accounts/register.html', {'form': form})
+    else:
+        form = RegisterForm()
+        return render(request, 'accounts/register.html',{'form':form})
+    
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login (request, user )
+
+            if user.role == 'voter':
+                return redirect('voter_dashboard')
+            elif user.role == 'admin':
+                return redirect('admin_dashboard')
+            elif user.role == 'candidate':
+                return redirect('candidate_dashboard')
+        else:
+            messages.error (request, "Invalid email/username or password.")
+            return render(request, 'accounts/login.html',{'form':form}) 
+               
+    else:
+        form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form':form})
+    
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
