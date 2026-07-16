0f0e1851d8566cad22bdc76fb1432f6d3d0055f1
Mandip <mandipadhikari.codes@gmail.com>
2026-07-16 21:11:18 +0545

    Use console email backend for local development

--- config/settings.py
+++ config/settings.py
@@ -17,16 +17,23 @@ load_dotenv()  # loads .env file
 from decouple import config
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-EMAIL_TIMEOUT = 10
 
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+if config("DJANGO_ENV", default="development") == "production":
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-DEFAULT_FROM_EMAIL = f"iVote <{EMAIL_HOST_USER}>"
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_TIMEOUT = 10
+
+    EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+    DEFAULT_FROM_EMAIL = f"iVote <{EMAIL_HOST_USER}>"
+
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "iVote <noreply@localhost>"
 
 
 
